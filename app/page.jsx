9f343cff3e7d311b6f0e5b7ce15e75c827942d79
Shack <g0972222165@gmail.com>
2024-02-12 11:11:26 +0800

    Add Foldes

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => (
+  <section className="w-full flex-center flex-col">
+    <h1 className="head_text text-center ">
+      Discover & Share
+      <br className="max-md:hidden " />
+      <span className="orange_gradient text-center "> 說出你的想法</span>
+    </h1>
+    <p className="desc text-center">
+      Promptopia 是開放可以分享的平台，讓你能發現、創作和分享你的創意想法
+    </p>
+  </section>
+);
+
+export default Home;
